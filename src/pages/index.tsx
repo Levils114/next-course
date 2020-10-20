eee325c98745c16e55e42ed3843e5ada7ec30573
Levils114 <levils114@gmail.com>
2020-10-20 13:51:22 -0300

    learning about fetch types, for example: server side redering, client side redering and static site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,44 @@
-export default function Home() {
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+
+import { Title } from './../styles/pages/Home';
+
+interface IRecommendedProducts{
+    id: number;
+    title: string;
+    price: number;
+    category_id: string;
+    slug: string;
+}
+
+interface IHome{
+    recommendedProducts: IRecommendedProducts[];
+}
+
+export default function Home({ recommendedProducts }: IHome) {
     return (
-        <div>
-            <a href="http://localhost:3000/search">Hello Next</a>
-        </div>
+    	<>
+    	<Head>
+    		<title>Next Course</title>
+    	</Head>
+
+        {recommendedProducts.map(product => (
+            <div key={product.id}>
+                <Title>{product.title}</Title>
+                <Title>R$ {product.price}</Title>
+            </div>
+        ))}
+        </>
     )
+}
+
+export const getServerSideProps: GetServerSideProps<IHome> = async () => {
+    const response = await fetch('http://localhost:3333/recommended');
+    const responseFormatted = await response.json();
+
+    return({
+        props: {
+            recommendedProducts: responseFormatted
+        }
+    });
 }
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+	*{
+		margin: 0;
+		padding: 0;
+	}
+
+	body{
+		background: #212121;
+		color: #F9F9F9;
+		font-family: Arial, Helvetica, sans-serif;
+		
+	}
+`;
